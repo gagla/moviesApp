ef4c2256f3efcbfb6f00c3db107ad74aeab60a81
aawi <agnieszka.wawrzyniak@gft.com>
2018-11-05 12:22:56 +0100

    mongoose conn error handling, Angular routing wild path added

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 
 mongoose.Promise = global.Promise;
 
-
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }).then(
     () => {
         console.log("connected to mongoDB")
